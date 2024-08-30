e4887cb6ddeb60ac5d3f76bbca3b0c240cc27946
JoelVR17 <vermudo.com@gmail.com>
2024-08-29 20:57:37 -0600

    feat: Add Footer and Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito, Nunito_Sans } from "next/font/google";
 import { createClient } from "@/prismicio";
 import clsx from "clsx";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -19,13 +21,14 @@ const nunitoSans = Nunito_Sans({
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
 
-  const page = await client.getSingle("settings");
+  const settings = await client.getSingle("settings");
 
   return {
-    title: page.data.site_title || "Flowrise Fallback",
-    description: page.data.meta_description || "Flowrise Fallback Description",
+    title: settings.data.site_title || "Flowrise Fallback",
+    description:
+      settings.data.meta_description || "Flowrise Fallback Description",
     openGraph: {
-      images: [page.data.og_image.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
   };
 }
@@ -38,9 +41,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
-        <header>Header</header>
+        <Header />
         {children}
-        <footer>Footer!</footer>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+
+const Footer = async () => {
+  const client = createClient();
+
+  const settings = await client.getSingle("settings");
+
+  return (
+    <>
+      <footer>
+        <Link href="/">{settings.data.site_title}</Link>
+
+        <p>&#169;{new Date().getFullYear()} - Joel Vargas</p>
+
+        <ul>
+          {settings.data.navigation.map(({ link, label }) => (
+            <li key={label}>
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
